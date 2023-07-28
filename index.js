93de681484e27f240511a5a63e11f213893ef881
Schemsou <chemseddinesediri@gmail.com>
2023-07-28 15:51:21 +0100

    working on register feature

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const userRouter = require("./routes/user");
-
+const authRouter = require("./routes/auth");
 const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.use("/api/user", userRouter);
-
+app.use("/api/auth", authRouter);
 app.listen(port, () => {
   console.log(`backend is running on port ${port}`);
 });
