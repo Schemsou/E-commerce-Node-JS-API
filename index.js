9fe2f5f5abfe4f3d664f9cf12067eecb09d33171
Schemsou <chemseddinesediri@gmail.com>
2023-07-28 23:16:52 +0100

    Operations on products and listing them according to category or newest

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
+const productRouter = require('./routes/products')
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/products', productRouter)
 app.listen(port, () => {
   console.log(`backend is running on port ${port}`);
 });
