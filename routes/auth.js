4e0a68667f1221fe4dcc6a63afea423acfc4e35a
Schemsou <chemseddinesediri@gmail.com>
2023-07-28 19:54:41 +0100

    update user and auth middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -30,8 +31,18 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(400).json({ message: "Wrong infos" });
 
+    const accesToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {
+        expiresIn: "30d",
+      }
+    );
     const { password, ...others } = user._doc;
-    res.status(201).json(others);
+    res.status(201).json({ ...others, accesToken });
   } catch {
     (err) => res.status(500).json(err);
   }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
+const {
+  verifyTokenAndAuthorization,
+  verifyToken,
+} = require("../middleware/verifyToken");
+const User = require("../models/User");
 
-router.get("/usertest", (req, res) => res.json({ message: "tested" }));
-
-router.post("/userpost", (req,res)=>{
-    const username = req.body.username;
-    res.status(200).json({ message: `Your name is ${username}` })
-})
-
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser)
+  } catch {
+    (err) => res.status(500).json(err);
+  }
+});
 module.exports = router;
